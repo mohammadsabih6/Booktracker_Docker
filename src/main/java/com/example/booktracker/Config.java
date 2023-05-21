3b259a9b90b82d5cadc1d7e9bfa4225c1c0cb383
mohammadsabih6 <sabih.tariq@xloopdigital.com>
2023-05-21 14:47:07 +0500

    ALL done

--- src/main/java/com/example/booktracker/Config.java
+++ src/main/java/com/example/booktracker/Config.java
@@ -2,7 +2,6 @@ package com.example.booktracker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Configuration;
 
 @EnableRabbit
@@ -12,9 +11,9 @@ public class Config {
     public Queue helloQueue() {
         return new Queue("hello");
     }
-	@RabbitListener(queues = "hello")
-	public void receiver(String recevier) {
-		System.out.println(" Received '" + recevier + "'");
-	}
+	// @RabbitListener(queues = "hello")
+	// public void receiver(String recevier) {
+	// 	System.out.println(" Received '" + recevier + "'");
+	// }
     
 }
